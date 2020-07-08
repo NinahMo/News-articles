7563952a99b3ab4b034505edcedb9a0e42c2bfe2
Ashley Mosoti <ninahb746@gmail.com>
2020-07-08 23:54:13 +0300

    use get_sources method to fetch api

--- app/main/views.py
+++ app/main/views.py
@@ -12,7 +12,7 @@ def index():
 	sources = get_sources('business')
 	sports_sources = get_sources('sports')
 	technology_sources = get_sources('technology')
-	entertainment_sources = get_sources('entertainment')
+	entertainment_sources = get_sources('entertainment')        
 	title = "News Highlighter"
 
 	return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
