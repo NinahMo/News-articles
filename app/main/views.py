9e7caf09c8f6b75aa7b4f185e916d11d205b2635
Ashley Mosoti <ninahb746@gmail.com>
2020-07-08 23:57:10 +0300

    create a views file

--- app/main/views.py
+++ app/main/views.py
@@ -10,9 +10,9 @@ def index():
 	view root page function that returns the index the page and its data
 	'''
 	sources = get_sources('business')
-	sports_sources = get_sources('sports')
+	sports_sources = get_sources('sports')      
 	technology_sources = get_sources('technology')
-	entertainment_sources = get_sources('entertainment')        
+	entertainment_sources = get_sources('entertainment')     
 	title = "News Highlighter"
 
 	return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
